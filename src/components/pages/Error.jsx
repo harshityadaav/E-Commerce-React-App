4a6e13b131518a369eb51da450e6a7437cf277ee
harshityadaav <105654252+harshityadaav@users.noreply.github.com>
2023-04-10 05:31:42 +0530

    Second Commit

--- src/components/pages/Error.jsx
+++ src/components/pages/Error.jsx
@@ -2,7 +2,7 @@ function Error() {
   return (
     <>
       <div className="mt-5 w-100 font-weight-bolder d-flex align-items-center justify-content-center">
-        <h1 className="p-5 mt-5 display-1 font-weight-bolder">Error 404</h1>
+        <h1 className="p-5 mt-5 display-1 font-weight-bolder">Welcome to my Website</h1>
         {/* <h2 className="display-4">Error 404 !!!</h2> */}
 
       </div>
